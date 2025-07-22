ec20a8589cbc026b330fbbc65fc847142aee15bc
Mason Ritchason <masonritchason@gmail.com>
2025-07-22 12:06:32 -0400

    fix: add direct route in Actions workflow environment
    Signed-off-by: Mason Ritchason <masonritchason@gmail.com>

--- LotComPrinterSetup/Program.cs
+++ LotComPrinterSetup/Program.cs
@@ -85,8 +85,7 @@ namespace LotComPrinterSetup
             {
                 return;
             }
-            // set to the directory of the Project folder (side-loaded in <Project>\LotCoMPrinter\Setup)
-            Environment.CurrentDirectory = @"..\bin\Release\net9.0-windows10.0.19041.0\win10-x64";  // setup project script home
+            Environment.CurrentDirectory = @"D:\a\LotCom-printer\LotCom-printer\LotComPrinter\bin\Release\net9.0-windows10.0.19041.0\win10-x64";  // setup project script home
             Environment.SetEnvironmentVariable("LATEST_RELEASE", VersionNumber);
             // generates a new GUID for the specific version installation
             Guid ProductId = GenerateProductId($"LotCom Printer {VersionNumber}");  // Do not change
